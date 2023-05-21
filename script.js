0b799f6e7d90b3838338d47490463aac1de13a73
kdrai007 <987kundanrai@gmail.com>
2023-05-22 01:55:24 +0530

    done with minimal design and features

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 const socket=io("http://localhost:3000");
+const messageForm=document.querySelector(".submit-conversation");
+const messageInput =document.querySelector("#message-input");
+const chatConversatioin =document.querySelector("#chat-conversation");
 
+const Username=prompt("Enter your name");
+appendMessage("you joined");
+socket.emit("new-user",Username);
 
 socket.on("chat-message",data=>{
     console.log(data);
+    appendMessage(`${data.user}:${data.msg}`);
 })
+socket.on("user-name",name=>{
+    appendMessage(`${name} connected`);
+})
+socket.on("user-disconnected",name=>{
+    appendMessage(`${name} disconnected`);
+})
+
+messageForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const message=messageInput.value;
+    appendMessage(`you:${message}`);
+    socket.emit("send-chat-message",message);
+    messageInput.value="";
+})
+
+function appendMessage(message){
+    const messageElm=document.createElement("div");
+    messageElm.innerText=message;
+    chatConversatioin.appendChild(messageElm);
+}
--- server.js
+++ server.js
@@ -1,7 +1,23 @@
-const io = require('socket.io')(3000);
+const io = require('socket.io')(3000,{
+    cors:{
+        origin:"*",
+    }
+});
+const users={};
 
 io.on('connection', (socket) => {
-    console.log("new user");
-  socket.emit("chat-message","kdrai is me");
+    socket.on("new-user",Username=>{
+        users[socket.id]=Username;
+        console.log(users);
+        socket.broadcast.emit("user-name",Username);
+    })
+  socket.on("send-chat-message",message=>{
+    console.log(users);
+    socket.broadcast.emit("chat-message",{msg:message,user:users[socket.id]});
+  })
+  socket.on("disconnect",()=>{
+    socket.broadcast.emit("user-disconnected",users[socket.id]);
+    delete users[socket.id];
+  })
 });
 
